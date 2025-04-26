147daebe8b49571be2d26baa2c04e2ad4d7d2665
rahulbingi24 <rahulbingi248@gmail.com>
2025-04-26 21:44:01 +0530

    ChangeBgColor

---
+++ Changebgcolor/script.js
@@ -0,0 +1,23 @@
+document.querySelector('#color1').addEventListener('click',()=>{
+    document.querySelector('body').style.backgroundColor = "Red";
+})
+
+document.querySelector('#color2').addEventListener('click',()=>{
+    document.querySelector('body').style.backgroundColor = "blue";
+})
+
+document.querySelector('#color3').addEventListener('click',()=>{
+    document.querySelector('body').style.backgroundColor = "green";
+})
+
+document.querySelector('#color4').addEventListener('click',()=>{
+    document.querySelector('body').style.backgroundColor = "gold";
+})
+
+document.querySelector('#color5').addEventListener('click',()=>{
+    document.querySelector('body').style.backgroundColor = "tomato";
+})
+
+document.querySelector('#color6').addEventListener('click',()=>{
+    document.querySelector('body').style.backgroundColor = "khaki";
+})
